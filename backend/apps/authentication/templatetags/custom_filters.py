4134c0606b14c13954c0fc1a8905200e7c2f68b5
camille-004 <dunningcamille@gmail.com>
2024-05-27 00:16:03 -0700

    Mypy rules.

--- backend/apps/authentication/templatetags/custom_filters.py
+++ backend/apps/authentication/templatetags/custom_filters.py
@@ -1,14 +1,13 @@
 import re
 
 from django import template
-from django.utils.safestring import mark_safe
+from django.utils.safestring import SafeString, mark_safe
 
 register = template.Library()
 
 
 @register.filter
-def exclude_file_input_text(value):
-    print("Filter accessed with value:", value)
+def exclude_file_input_text(value: str | SafeString) -> str | SafeString:
     if isinstance(value, str):
         cleaned_value = re.sub(
             r"Currently:.*?<br>\s*Change:", "Change:", value, flags=re.DOTALL
